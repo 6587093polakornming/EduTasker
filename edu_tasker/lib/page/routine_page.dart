e3a050eec7b657a1d6c069006f022e3474b3672a
Polakorn Anantapakorn <polakorn.ana@student.mahidol.edu>
2024-03-31 18:24:13 +0700

    Update RoutinePage

--- edu_tasker/lib/page/routine_page.dart
+++ edu_tasker/lib/page/routine_page.dart
@@ -1,10 +1,16 @@
 import 'package:edu_tasker_app/models/routine_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 void main(List<String> args) {
-  runApp(const RoutinePage());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Flutter App",
+    home: RoutinePage(), //error The error you’re encountering is due to the absence of MaterialLocalization
+    //Floating Button under MaterialApp
+  ));
 }
 
 class RoutinePage extends StatefulWidget {
@@ -25,80 +31,287 @@ class _RoutinePageState extends State<RoutinePage> {
     routines = RoutineModel.getRoutines();
   }
 
+  void increaseCount(int index) {
+    setState(() {
+      routines[index].count++;
+    });
+  }
+
+  void decreaseCount(int index) {
+    setState(() {
+      if (routines[index].count > 0) {
+        routines[index].count--;
+      }
+    });
+  }
+
+  void deleteRoutine(int index) {
+    setState(() {
+      routines.removeAt(index);
+    });
+  }
+
+  void addRoutine(String name, String description, int count, String unit) {
+    routines.add(RoutineModel(
+        name: name, description: description, count: count, unit: unit));
+  }
+
+  Future<void> showDeleteConfirmationDialog(
+      BuildContext context, int index) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // ไม่ให้ปิด dialog โดยการแตะภายนอก
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('คุณแน่ใจหรือไม่?'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('คุณต้องการลบ Routine นี้หรือไม่?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('ยกเลิก'),
+              onPressed: () {
+                Navigator.of(context).pop(); // ปิด dialog
+              },
+            ),
+            TextButton(
+              child: Text('ตกลง'),
+              onPressed: () {
+                deleteRoutine(
+                    index); // เรียกฟังก์ชัน deleteRoutine เมื่อผู้ใช้กดตกลง
+                Navigator.of(context).pop(); // ปิด dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> showAddRoutineDialog(BuildContext context) async {
+    String name = '';
+    int count = 0;
+    String unit = '';
+    String description = '';
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // ไม่ให้ปิด dialog โดยการแตะภายนอก
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('เพิ่ม Routine'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  onChanged: (value) => name = value,
+                  decoration: InputDecoration(labelText: 'ชื่อ Routine'),
+                ),
+                TextField(
+                  onChanged: (value) => count = int.tryParse(value) ?? 0,
+                  decoration: InputDecoration(labelText: 'จำนวน Routine'),
+                  keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  onChanged: (value) => unit = value,
+                  decoration: InputDecoration(labelText: 'หน่วย Routine'),
+                ),
+                TextField(
+                  onChanged: (value) => description = value,
+                  decoration: InputDecoration(labelText: 'รายละเอียด Routine'),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('ยกเลิก'),
+              onPressed: () {
+                Navigator.of(context).pop(); // ปิด dialog
+              },
+            ),
+            TextButton(
+              child: Text('ตกลง'),
+              onPressed: () {
+                // เพิ่ม Routine โดยเรียกฟังก์ชันที่กำหนดไว้ที่บรรทัดที่ 3 โดยส่งพารามิเตอร์ข้อมูลของ Routine
+                addRoutine(name, description, count, unit);
+                Navigator.of(context).pop(); // ปิด dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter App',
-      home: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
+    return 
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
             begin: Alignment.topCenter,
-            end:Alignment.bottomCenter,
-            colors: [Color.fromARGB(255, 55, 29, 100), Color.fromARGB(255,20, 47, 114)],
-            )
-         ),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: getAppBar(),
-          body: Column(
-            children: [
-              // Text("This is home page",style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)),
-              SearchBox(),
-              // routine section
-              SizedBox(height: 32),
-              Container(
-                // color: Colors.red,
-                height: 450,
-                child: ListView.separated(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: routines.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      tileColor: Colors.white,
-                      subtitle: Text(routines[index].description),
-                      title: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(routines[index].name),
-                          Text('${routines[index].count.toString()}/${routines[index].unit}'),
-                        ],
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) => const Divider(), 
-          
-                ),
-              ),
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 55, 29, 100),
+              Color.fromARGB(255, 20, 47, 114)
             ],
+          )),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: getAppBar(),
+            body: Column(
+              children: [
+                // Text("This is home page",style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)),
+                SearchBox(),
+    
+                // routine section
+                SizedBox(height: 32),
+                Container(
+                  margin: EdgeInsets.only(right: 28, left: 28),
+                  // color: Colors.red,
+                  height: 450,
+                  child: ListView.separated(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: routines.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          tileColor: Colors.white,
+                          //subtitle: Text(routines[index].description),
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              //Remove counter Button
+                              ElevatedButton(
+                                onPressed: () {
+                                  // code when button is pressed
+                                  decreaseCount(index);
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.redAccent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  padding: EdgeInsets
+                                      .zero, // เปลี่ยนเป็น EdgeInsets.zero เพื่อไม่ให้มีพื้นที่ว่างรอบปุ่ม
+                                  minimumSize:
+                                      Size(48, 48), // กำหนดขนาดปุ่มเป็น 48x48
+                                ),
+                                child: Icon(
+                                  Icons.remove,
+                                  color: Colors.white,
+                                  size: 24,
+                                ),
+                              ),
+                              // Routine Infomation Section
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        routines[index].name,
+                                        style: TextStyle(fontSize: 14),
+                                      ), //Show Name of Routine
+                                      SizedBox(
+                                        width: 8,
+                                      ),
+                                      Text(
+                                          '${routines[index].count.toString()}/${routines[index].unit}',
+                                          style: TextStyle(
+                                              fontSize:
+                                                  14)), //Show Counting and unit of Routine
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 6,
+                                  ),
+                                  Text(routines[index].description,
+                                      style: TextStyle(fontSize: 14)),
+                                ],
+                              ),
+                              //Add Counting Button
+                              ElevatedButton(
+                                onPressed: () {
+                                  // code when button is pressed
+                                  increaseCount(index);
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.greenAccent,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  padding: EdgeInsets
+                                      .zero, // เปลี่ยนเป็น EdgeInsets.zero เพื่อไม่ให้มีพื้นที่ว่างรอบปุ่ม
+                                  minimumSize:
+                                      Size(48, 48), // กำหนดขนาดปุ่มเป็น 48x48
+                                ),
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                  size: 24,
+                                ),
+                              ),
+                            ],
+                          ),
+                          trailing: Icon(Icons.delete, color: Colors.redAccent),
+                          onTap: () {
+                            showDeleteConfirmationDialog(context, index);
+                          },
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) =>
+                          const SizedBox(
+                            height: 24,
+                          )),
+                ),
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                showAddRoutineDialog(
+                    context); // เรียกฟังก์ชันเพื่อแสดง dialog เพิ่ม Routine
+              },
+              child: Icon(Icons.add),
+              backgroundColor: Colors.greenAccent,
+            ),
           ),
-        ),
-      )
-    );
+        );
   }
 
   AppBar getAppBar() {
     return AppBar(
-          actions: [
-            IconButton(icon: Icon(Icons.settings, color: Colors.white, size: 40.0) , onPressed: (){})
-          ],
-          centerTitle: true,
-          title: Center(
-            child: Text("Routine", 
-            style: TextStyle(color: Colors.white, fontSize: 34),
-            ),
+      actions: [
+        IconButton(
+            icon: Icon(Icons.settings, color: Colors.white, size: 40.0),
+            onPressed: () {})
+      ],
+      centerTitle: true,
+      title: Center(
+        child: Text(
+          "Routine",
+          style: TextStyle(color: Colors.white, fontSize: 34),
+        ),
+      ),
+      leading: Container(
+        // color: Colors.red,
+        margin: EdgeInsets.only(left: 8.00, bottom: 8.00, top: 10.00),
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          backgroundImage: AssetImage(
+            'assets/default_avatar.png',
           ),
-          leading: Container(
-            // color: Colors.red,
-            margin:EdgeInsets.only(left: 8.00, bottom: 8.00, top: 10.00),
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              backgroundImage: AssetImage('assets/default_avatar.png',),
-              // radius: 200.0,
-              ),
-          ),
-          backgroundColor: Color.fromARGB(255, 24, 28, 75),
-        );
+          // radius: 200.0,
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 24, 28, 75),
+    );
   }
 }
 
